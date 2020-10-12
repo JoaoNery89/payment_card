f1a02087da648259343b698be6fe4b2795e8b67e
JoaoNery89 <joao.nery89@outlook.com>
2020-10-12 18:40:31 -0300

    some layout adjustmets in both cards

--- lib/myCard.dart
+++ lib/myCard.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 class MyCard extends StatefulWidget {
-  MyCard({Key key, this.title}) : super(key: key);
+  MyCard({Key key, this.title, this.type, this.number, this.name, this.month}) : super(key: key);
 
   final String title;
+  CardType type;
+  String number;
+  String name;
+  int month;
+  int year;
+  int cvv;
 
   @override
   _MyCardState createState() => _MyCardState();
@@ -23,14 +29,15 @@ class _MyCardState extends State<MyCard> {
         backgroundColor: Colors.white,
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.only(bottom: bottom),
+        padding: EdgeInsets.only(bottom: bottom,),
         reverse: true,
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(12.0,12.0,12.0,0),
             child: Column(
               children: [
-                Card(
+                Card( //Todo: Separar em um componente
+                  shadowColor: Colors.lightBlue,
                   child: Stack(
                     children: <Widget>[
                       Image.asset('assets/cardBg.png',fit: BoxFit.cover,),
@@ -76,26 +83,29 @@ class _MyCardState extends State<MyCard> {
                                     )
                                   ],
                                 ),
-                                Column(
-                                  children: <Widget>[
-                                    Text(
-                                      "CVV",
-                                      style: TextStyle(
+                                Padding(
+                                  padding: const EdgeInsets.only(left:50),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(
+                                        "CVV",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 22.0
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        "***",
+                                        style: TextStyle(
                                           color: Colors.white,
-                                          fontSize: 22.0
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 10.0,
-                                    ),
-                                    Text(
-                                      "***",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18.0,
-                                      ),
-                                    )
-                                  ],
+                                          fontSize: 18.0,
+                                        ),
+                                      )
+                                    ],
+                                  ),
                                 )
                               ],
                             ),
@@ -103,7 +113,7 @@ class _MyCardState extends State<MyCard> {
                               height: 8.0,
                             ),
                             Text(
-                              "Titular",
+                              "Meu Nome",
                               style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 28.0
@@ -119,7 +129,91 @@ class _MyCardState extends State<MyCard> {
                   clipBehavior: Clip.antiAliasWithSaveLayer,
                   semanticContainer: true,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0)
+                    borderRadius: BorderRadius.circular(5.0)
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16,),
+                  child: Divider(
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                      //border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black38,
+                          blurRadius: 4.0,
+                          spreadRadius: 0.0
+                        ),
+                      ]
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Número",
+                              suffixIcon: Icon(
+                                Icons.credit_card
+                              )
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Validade do Cartão",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Código de Segurança",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Nome do Titular",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24),
+                  child: FlatButton(
+                    color: Colors.blue,
+                    onPressed: (){},
+                    shape: StadiumBorder(),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 12.0),
+                      child: Text(
+                        "Cadastrar",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.0
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -130,4 +224,11 @@ class _MyCardState extends State<MyCard> {
     );
   }
 
+}
+
+enum CardType {
+  MasterCard,
+  Visa,
+  Others, // Any other card issuer
+  Invalid // We'll use this when the card is invalid
 }
